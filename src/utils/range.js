0738ece2a83c22d0cbc8b80eb5b6b39f2df47f93
Антоний Гончаров <60212039@leroymerlin.ru>
2024-12-06 14:53:45 +0300

    new line files

--- src/utils/range.js
+++ src/utils/range.js
@@ -1,13 +1,3 @@
-/*
-  * range(4); // => [0, 1, 2, 3] 
-  * range(-4); // => [0, -1, -2, -3]
-  * range(1, 5); // => [1, 2, 3, 4]
-  * range(0, 20, 5); // => [0, 5, 10, 15]
-  * range(0, -4, -1); // => [0, -1, -2, -3]
-  * range(1, 4, 0); // => [1, 1, 1]
-  * range(0); // => []
-*/
-
 function range(start = 0, end, step = 1) {
     const array = [];
     if (arguments.length === 1) {
