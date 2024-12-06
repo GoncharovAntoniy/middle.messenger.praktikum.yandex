0ad1020acdcff9c207d741d5741078f5d2a24f07
Антоний Гончаров <60212039@leroymerlin.ru>
2024-12-06 15:24:20 +0300

    packages update and vite v 4.3.9

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import { viteStaticCopy } from "vite-plugin-static-copy";
-
 
 export default defineConfig({
     build: {
@@ -11,15 +9,5 @@ export default defineConfig({
             },
         },
     },
-    plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: "public/images/**/*", // Пусть к изображениям в папке public/images
-                    dest: "images", // Куда копировать изображения в папке dist
-                },
-            ],
-        }),
-    ],
     assetsInclude: ['**/*.hbs, **/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.webp'], 
 })
