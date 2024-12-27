5067d0140e555d2b0a3a734d9a0c25e848189cb3
Антоний Гончаров <60212039@leroymerlin.ru>
2024-12-27 15:22:39 +0300

    eslint config prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'eslint-config-prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
+    'prettier/prettier': ['error', { endOfLine: 'crlf' }],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
     'no-unused-vars': 'off',
