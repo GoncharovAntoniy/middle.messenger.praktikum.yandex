5e54403e03079ba65445f5ea214ae08a9410575f
Антоний Гончаров <60212039@leroymerlin.ru>
2024-12-29 18:48:11 +0300

    fix isEmpty prettier

--- src/utils/isEmpty.js
+++ src/utils/isEmpty.js
@@ -1,7 +1,5 @@
 function isLength(value) {
-  return (
-    typeof value === 'number' && value > -1 && value % 1 === 0 && value <= Number.MAX_SAFE_INTEGER
-  );
+  return typeof value === 'number' && value > -1 && value % 1 === 0 && value <= Number.MAX_SAFE_INTEGER;
 }
 
 function isNil(value) {
