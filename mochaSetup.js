0210c69f1fadb0bce60e9bbe4462c9e5f5465798
Антоний Гончаров <60212039@lemanapro.ru>
2025-04-05 17:45:15 +0300

    create tests router and HTTPTransport

--- mochaSetup.js
+++ mochaSetup.js
@@ -7,3 +7,17 @@ global.window = jsdom.window;
 global.document = jsdom.window.document;
 global.MouseEvent = jsdom.window.MouseEvent;
 global.Node = jsdom.window.Node;
+global.location = jsdom.window.location;
+
+// Переопределяем методы history для тестов
+jsdom.window.history.pushState = function () {
+  // Пустая реализация для тестов
+};
+
+jsdom.window.history.back = function () {
+  // Пустая реализация для тестов
+};
+
+jsdom.window.history.forward = function () {
+  // Пустая реализация для тестов
+};
